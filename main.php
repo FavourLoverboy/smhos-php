e986fa3a25f6097d646bc1d818b4df6e31a38706
favourloverboy <favourehio2019@gmail.com>
2022-10-31 19:51:02 +0100

    controlling users access page

--- main.php
+++ main.php
@@ -3,6 +3,31 @@
     if(!$_SESSION['myId']){
         header('location: /smhos-php/');
     }
+
+    $admin = 'admin';
+    $church = 'church';
+    $leader = 'leader';
+    $member = 'member';
+
+    if($_SESSION['level'] == 'A'){
+        if($url[0] != $admin){
+            header("location: /smhos-php/$admin/dashboard");
+        }
+    }elseif($_SESSION['level'] == 'C'){
+        if($url[0] != $church){
+            header("location: /smhos-php/$church/dashboard");
+        }
+    }elseif($_SESSION['level'] == 'L'){
+        if($url[0] != $leader){
+            header("location: /smhos-php/$leader/dashboard");
+        }
+    }else{
+        if($url[0] != $member){
+            header("location: /smhos-php/$member/dashboard");
+        }
+    }
+
+
 ?>
 
 <?php include('includes/main/header.php'); ?>
