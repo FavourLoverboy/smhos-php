e9ab9e612eccf52232093749d02e2605bf887add
favourloverboy <favourehio2019@gmail.com>
2022-10-31 15:42:55 +0100

    login pages for leaders functionality

--- leaders.php
+++ leaders.php
@@ -1,4 +1,68 @@
 <?php include('includes/authenticate/header.php');?>
+<?php 
+$errMessage = "";
+    if($_POST){
+        extract($_POST);
+        $encryptPassword = $connect->epass($password);
+
+        // Validating Email
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $_SESSION['validate_email'] = true;
+        }
+
+        if($_SESSION['validate_email'] == true){
+            // Checking for email or username
+            $tblquery = "SELECT * FROM tbl_login WHERE email = :email";
+            $tblvalue = array(
+                ':email' => htmlspecialchars($email)
+            );
+            $emailCheck = $connect->tbl_select($tblquery, $tblvalue);
+            if($emailCheck){
+                $tblquery = "SELECT * FROM tbl_login WHERE email = :email && password = :password";
+                $tblvalue = array(
+                    ':email' => htmlspecialchars($email),
+                    ':password' => htmlspecialchars($encryptPassword)
+                );
+                // print_r($tblvalue);
+                $login = $connect->tbl_select($tblquery, $tblvalue);
+                if($login){
+                    $tblquery = "SELECT * FROM tbl_login WHERE email = :email AND status = :status";
+                    $tblvalue = array(
+                        ':email' => htmlspecialchars($email),
+                        ':status' => '1'
+                    );
+                    // print_r($tblvalue);
+                    $statusCheck = $connect->tbl_select($tblquery, $tblvalue);
+                    if($statusCheck){
+                        foreach($statusCheck as $data){
+                            extract($data);
+                            $_SESSION['myId'] = $id;
+                            $_SESSION['email'] = $email;
+                            $_SESSION['level'] = $level;
+    
+                            if($_SESSION['level'] == 'A'){
+                                echo "<script>  window.location='admin/dashboard' </script>";
+                            }else if($_SESSION['level'] == 'C'){
+                                echo "<script>  window.location='church/dashboard' </script>";
+                            }else if($_SESSION['level'] == 'L'){
+                                echo "<script>  window.location='leader/dashboard' </script>";
+                            }
+                        }
+                    }else{
+                        $errMessage = "you account has been disabled, contact any of the higher admintratives for further information.";
+                    }
+                }else{
+                    $errMessage = "wrong login credentials";
+                }
+            }else{
+                $errMessage = "email don't exist";
+            }
+        }else{
+            $errMessage = 'invalid email';
+        }
+    }
+?>
     <div class="col-lg-6 d-none d-lg-block bg-login-image2"></div>
     <div class="col-lg-6">
         <div class="p-5">
@@ -32,7 +96,7 @@
                 ?>
                 
                 <div class="form-group">
-                    <input type="type" class="form-control form-control-user"
+                    <input type="email" class="form-control form-control-user"
                         id="exampleInputEmail" aria-describedby="emailHelp"
                         placeholder="Enter Your Email..." name="email" value="<?php echo $email; ?>">
                 </div>
--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if(!$_SESSION['username']){
+    if(!$_SESSION['myId']){
         header('location: login');
     }
 ?>
