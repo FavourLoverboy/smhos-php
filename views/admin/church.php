6c2882a335ef1fef6b6b73b383ecf9491917598b
favourloverboy <favourehio2019@gmail.com>
2022-11-01 14:11:52 +0100

    admin viewing churches

--- views/admin/church.php
+++ views/admin/church.php
@@ -1,1 +1,71 @@
-church
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="row">
+                <div class="col-3">
+                    <a href="/<%= page %>/add_church">
+                        <button type="button" class="btn btn-info ml-2">Add Church</button>
+                    </a>
+                </div>
+            </div>
+
+            <div class="card-header">
+                <h4 class="card-title">Churches</h4>
+                <% if(success){ %>
+                    <label style="color: green;"><%= success %></label>
+                <% } %>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class=" text-primary">
+                            <th>S/N</th>
+                            <th>Name</th>
+                            <th>LGA</th>
+                            <th>Country</th>
+                            <th>Homecells</th>
+                            <th class="text-right">View</th>
+                        </thead>
+                        <tbody>
+                            <?php
+                                $tblquery = "SELECT churches.id, churches.name, churches.lga, churches.country, COUNT(homecells.church_id) AS homecells FROM churches INNER JOIN homecells ON churches.id = homecells.church_id GROUP BY homecells.church_id ORDER BY homecells";
+                                $tblvalue = array();
+                                $select =$connect->tbl_select($tblquery, $tblvalue);
+                                $si = 1;
+                                if($select){
+                                    foreach($select as $data){
+                                        extract($data);
+                                        ?>
+                                        <?php
+                                            echo "
+                                                <tr>
+                                                    <td>$si</td>
+                                                    <td>$name</td>
+                                                    <td>$lga</td>
+                                                    <td>$country</td>
+                                                    <td>$homecells</td>
+                                                    <td>
+                                                        <form action='' method='POST'>
+                                                            <input type='hidden' name='id' value='$id'>
+                                                            <button class='btn btn-info btn-sm'>view</button>
+                                                        </form>
+                                                    </td>
+                                                </tr>
+                                            ";
+                                            $si++;
+                                    }
+                                }else{
+                                    echo "
+                                        <tr>
+                                            <td colspan='6'>There is no Church</td>
+                                        </tr>
+                                    ";
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
