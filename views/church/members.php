94ddaae737de98e2e5136115ddb5e53639b537c0
favourloverboy <favourehio2019@gmail.com>
2022-11-14 20:05:20 +0100

    church view members

---
+++ views/church/members.php
@@ -0,0 +1,84 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="row">
+                <div class="col-3">
+                    <a href="add_member">
+                        <button type="button" class="btn btn-info ml-2">Add Member</button>
+                    </a>
+                </div>
+            </div>
+
+            <div class="card-header">
+                <h4 class="card-title">Members</h4>
+                <?php 
+                    if($_SESSION['Message']){
+                        echo "
+                            <label style='color: green;font-size:20px;'>$_SESSION[Message]</label>
+                        ";
+                    }
+                
+                ?>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class=" text-primary">
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Sex</th>
+                            <th>Number</th>
+                            <th>Marital Status</th>
+                            <th class="text-right">View</th>
+                        </thead>
+                        <tbody>
+                            <?php
+                                
+                                $tblquery = "SELECT * FROM members WHERE church_id = :id ORDER BY last_name";
+                                $tblvalue = array(
+                                    ':id' => $_SESSION['church_id']
+                                );
+                                $select = $connect->tbl_select($tblquery, $tblvalue);
+                                if($select){
+                                    foreach($select as $data){
+                                        extract($data);
+                                        echo "
+                                            <tr>
+                                                <td>$last_name $first_name</td>
+                                                <td>$email</td>
+                                                <td>$sex</td>
+                                                <td>$number</td>
+                                                <td>$marital_status</td>
+                                                <td class='text-right'>
+                                                    <form method='post' action=''>
+                                                        <input type='hidden' name='member_id' value='$id'>
+                                                        <input type='submit' class='btn btn-success btn-sm' value='view'>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        ";
+                                    }
+                                }else{
+                                    echo "
+                                        <tr>
+                                            <td colspan='6'>There is no Member</td>
+                                        </tr>
+                                    ";
+                                }                              
+                            
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    $_SESSION['Message'] = '';
+    if($_POST){
+        extract($_POST);
+        $_SESSION['view_member_id'] = $member_id;
+        echo "<script>  window.location='view_member' </script>";
+    }
+?>
