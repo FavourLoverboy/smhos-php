8bc05790534c5e246e095b4dfc4a3bcb3e5c4c14
favourloverboy <favourehio2019@gmail.com>
2022-11-16 18:12:43 +0100

    member view profile

--- includes/navs/level/member.php
+++ includes/navs/level/member.php
@@ -1,11 +1,11 @@
 <li class="active ">
-    <a href="<?php echo $url[0]; ?>/dashboard">
+    <a href="dashboard">
         <i class="nc-icon nc-bank"></i>
         <p>Dashboard</p>
     </a>
 </li>
 <li>
-    <a href="<?php echo $url[0]; ?>/profile">
+    <a href="profile">
         <i class="nc-icon nc-shop"></i>
         <p>My Profile</p>
     </a>
