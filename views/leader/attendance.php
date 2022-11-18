8806f44be6a9d582b5352cada5dc192631d6c336
favourloverboy <favourehio2019@gmail.com>
2022-11-18 18:20:13 +0100

    leader other attendance

--- views/leader/attendance.php
+++ views/leader/attendance.php
@@ -1,21 +1,64 @@
 <?php
     $_SESSION['Message'] = '';
-    if($_POST){
+    $_SESSION['MessageOther'] = '';
+    $_SESSION['lname'] = '';
+    $_SESSION['fname'] =  '';
+    if($_POST['sign']){
         extract($_POST);
 
-        $tblquery = "INSERT INTO attendance VALUES(:id, :addedBy, :user, :c_id, :h_id, :theme_id, :date)";
         $tblquery = "UPDATE attendance SET addedBy = :ad, h_id = :hi, date = :date WHERE user = :user AND theme_id = :theme_id";
         $tblvalue = array(
             ':ad' => $_SESSION['myId'],
-            ':hi' => htmlspecialchars($homecell_id),
+            ':hi' => htmlspecialchars($_SESSION['homecell_id']),
             ':date' => date("Y-m-d h:i:s"),
             ':user' => htmlspecialchars($member_id),
             ':theme_id' => htmlspecialchars($_SESSION['theme_id'])
         );
-        $insert = $connect->tbl_insert($tblquery, $tblvalue);
-        if($insert){
+        $update = $connect->tbl_update($tblquery, $tblvalue);
+        if($update){
             $_SESSION['Message'] = "$member_name just sign in";
-            echo "<script>  window.location='attendance' </script>";
+            $select123 = '';
+            // echo "<script>  window.location='attendance' </script>";
+        }
+    }
+
+    if($_POST['proceed']){
+        extract($_POST);
+
+        $_SESSION['lname'] = $lname;
+        $_SESSION['fname'] =  $fname;
+
+        $tblquery = "SELECT * FROM members WHERE last_name = :ln AND first_name = :fn ORDER BY last_name";
+        $tblvalue = array(
+            ':ln' => htmlspecialchars(ucwords($lname)),
+            ':fn' => htmlspecialchars(ucwords($fname))
+        );
+        $select123 = $connect->tbl_select($tblquery, $tblvalue);
+        if($select123){
+            foreach($select123 as $data){
+                extract($data);
+                $tblquery = "SELECT * FROM churches WHERE id = :id";
+                $tblvalue = array(
+                    ':id' => $church_id
+                );
+                $church = $connect->tbl_select($tblquery, $tblvalue);
+                foreach($church as $data){
+                    extract($data);
+                    $_SESSION['church_name'] = $name;
+                }
+
+                $tblquery = "SELECT * FROM homecells WHERE id = :id";
+                $tblvalue = array(
+                    ':id' => $homecell_id
+                );
+                $homecell = $connect->tbl_select($tblquery, $tblvalue);
+                foreach($homecell as $data){
+                    extract($data);
+                    $_SESSION['homecell_name'] = $name;
+                }
+            }
+        }else{
+            $_SESSION['MessageOther'] = 'No match for the name';
         }
     }
 ?>
@@ -71,8 +114,7 @@
                                                     <form method='post' action=''>
                                                         <input type='hidden' name='member_id' value='$id'>
                                                         <input type='hidden' name='member_name' value='$last_name $first_name $other_name'>
-                                                        <input type='hidden' name='homecell_id' value='$homecell_id'>
-                                                        <input type='submit' class='btn btn-success btn-sm' value='sign in'>
+                                                        <input type='submit' name='sign' class='btn btn-success btn-sm' value='sign in'>
                                                     </form>
                                                 </td>
                                             </tr>
@@ -81,7 +123,7 @@
                                 }else{
                                     echo "
                                         <tr>
-                                            <td colspan='6'>No More request</td>
+                                            <td colspan='6'>All signed in</td>
                                         </tr>
                                     ";
                                 }                              
@@ -94,3 +136,91 @@
         </div>
     </div>
 </div>
+
+<?php 
+
+    if($select123){
+        echo "
+            <div class='row'>
+                <div class='col-md-12'>
+                    <div class='card'>
+                        <div class='card-header'>
+                            <h5 class='card-title'>Best Match</h5>
+                        </div>
+                        <div class='card-body'>
+                            <div class='table-responsive'>
+                                <table class='table'>
+                                    <thead class=' text-primary'>
+                                        <th>Name</th>
+                                        <th>Church</th>
+                                        <th>Homecell</th>
+                                        <th>Sign In</th>
+                                    </thead>
+                                    <tbody>
+        ";
+        
+        foreach($select123 as $data){
+            extract($data);
+            echo "
+                <tr>
+                    <td>$last_name $first_name $other_name</td>
+                    <td>$_SESSION[church_name]</td>
+                    <td>$_SESSION[homecell_name]</td>
+                    <td>
+                        <form method='post' action=''>
+                            <input type='hidden' name='member_id' value='$id'>
+                            <input type='hidden' name='member_name' value='$last_name $first_name $other_name'>
+                            <input type='submit' name='sign' class='btn btn-success btn-sm' value='sign in'>
+                        </form>
+                    </td>
+                </tr>
+            ";
+        }
+        echo "
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ";
+    }else{
+        echo "
+            <div class='row'>
+                <div class='col-md-12'>
+                    <div class='card card-user'>
+                        <div class='card-header'>
+                            <h5 class='card-title'>Other attendance</h5>
+                            <label style='color: red;font-size:20px;'>$_SESSION[MessageOther]</label>
+                        </div>
+                        <div class='card-body'>
+                            <form action='' method='post'>
+                                <div class='row'>
+                                    <div class='col-md-6 pl-3'>
+                                        <div class='form-group'>
+                                            <label for='name'>Last Name</label>
+                                            <input type='text' class='form-control' name='lname' placeholder='enter last mame' value='$_SESSION[lname]' required>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-6 pr-3'>
+                                        <div class='form-group'>
+                                            <label for='name'>First Name</label>
+                                            <input type='text' class='form-control' name='fname' placeholder='enter first name' value='$_SESSION[fname]' required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class='row'>
+                                    <div class='update ml-auto mr-auto'>
+                                        <input type='submit' name='proceed' class='btn btn-success btn-round' value='Proceed'>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ";
+    }
+
+?>
