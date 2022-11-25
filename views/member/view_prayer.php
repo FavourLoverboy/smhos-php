a0ac0b300fe2166c31212b1f8e117ceff4244df1
favourloverboy <favourehio2019@gmail.com>
2022-11-25 11:22:09 +0100

    members view prayer

---
+++ views/member/view_prayer.php
@@ -0,0 +1,16 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card card-user">
+            <div class="card-header">
+                <h5 class="card-title"><?php echo$_SESSION['view_theme']; ?></h5>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-12 pl-3">
+                        <?php echo $_SESSION['view_content']; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
