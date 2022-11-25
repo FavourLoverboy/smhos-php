9c2b47a377cfd9fe81ece5c0d4d36e12cc8d421d
favourloverboy <favourehio2019@gmail.com>
2022-11-25 23:17:11 +0100

    church dashboard icons

--- includes/navs/level/church.php
+++ includes/navs/level/church.php
@@ -1,30 +1,30 @@
 <li class="active ">
     <a href="dashboard">
-        <i class="nc-icon nc-bank"></i>
+        <i class="fa fa-dashboard"></i>
         <p>Dashboard</p>
     </a>
 </li>
 <li>
     <a href="attendance">
-        <i class="nc-icon nc-shop"></i>
+        <i class="fa fa-bar-chart"></i>
         <p>Attendance</p>
     </a>
 </li>
 <li>
     <a href="complains">
-        <i class="nc-icon nc-shop"></i>
+        <i class="fa fa-comments"></i>
         <p>Complains</p>
     </a>
 </li>
 <li>
     <a href="homecells">
-        <i class="nc-icon nc-shop"></i>
+        <i class="fa fa-home"></i>
         <p>Homecells</p>
     </a>
 </li>
 <li>
     <a href="members">
-        <i class="nc-icon nc-shop"></i>
+        <i class="fa fa-users"></i>
         <p>Members</p>
     </a>
 </li>
