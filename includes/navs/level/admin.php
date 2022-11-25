33b85fc880562d0f26af344981ae63cea139307f
favourloverboy <favourehio2019@gmail.com>
2022-11-25 23:13:43 +0100

    admin dashboard icons

--- includes/navs/level/admin.php
+++ includes/navs/level/admin.php
@@ -1,48 +1,48 @@
 <li class="active">
     <a href="dashboard">
-        <i class="nc-icon nc-bank"></i>
+        <i class="fa fa-dashboard"></i>
         <p>Dashboard</p>
     </a>
 </li>
 <li>
     <a href="churches">
-        <i class="nc-icon nc-shop"></i>
+        <i class="fa fa-university"></i>
         <p>Churches</p>
     </a>
 </li>
 <li>
     <a href="complains">
-        <i class="nc-icon nc-shop"></i>
+        <i class="fa fa-comments"></i>
         <p>Complains</p>
     </a>
 </li>
 <li>
     <a href="homecells">
-        <i class="nc-icon nc-shop"></i>
+        <i class="fa fa-home"></i>
         <p>Homecells</p>
     </a>
 </li>
 <li>
     <a href="members">
-        <i class="nc-icon nc-shop"></i>
+        <i class="fa fa-users"></i>
         <p>Members</p>
     </a>
 </li>
 <li>
     <a href="all_prayers">
-        <i class="nc-icon nc-shop"></i>
+        <i class="fa fa-bullhorn"></i>
         <p>Prayers</p>
     </a>
 </li>
 <li>
     <a href="testimonies">
-        <i class="nc-icon nc-shop"></i>
+        <i class="fa fa-smile-o"></i>
         <p>Testimonies</p>
     </a>
 </li>
 <li>
     <a href="themes">
-        <i class="nc-icon nc-shop"></i>
+        <i class="fa fa-book"></i>
         <p>Themes</p>
     </a>
 </li>
