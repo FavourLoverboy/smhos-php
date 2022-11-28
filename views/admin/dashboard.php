0036c9e59be1c2458766c6b60ece7d95acd55a7c
favourloverboy <favourehio2019@gmail.com>
2022-11-29 00:10:15 +0100

    admin dashboard first step

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -1,1 +1,234 @@
-admin dashboard
+<div class="row">
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-body ">
+                <div class="row">
+                    <div class="col-5 col-md-4">
+                        <div class="icon-big text-center icon-warning">
+                            <i class="fa fa-users text-warning"></i>
+                        </div>
+                    </div>
+                    <div class="col-7 col-md-8">
+                        <div class="numbers">
+                            <p class="card-category">Members</p>
+                            <?php
+                            
+                                $tblquery = "SELECT COUNT(id) AS allMembers FROM members";
+                                $tblvalue = array();
+                                $select = $connect->tbl_select($tblquery, $tblvalue);
+                                foreach($select as $data){
+                                    extract($data);
+                                    echo "
+                                        <p class='card-title'>$allMembers<p>
+                                    ";
+                                }
+                            
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                    <i class="fa fa-clock-o"></i>
+                    All Members
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-body ">
+                <div class="row">
+                    <div class="col-5 col-md-4">
+                        <div class="icon-big text-center icon-warning">
+                            <i class="fa fa-home text-success"></i>
+                        </div>
+                    </div>
+                    <div class="col-7 col-md-8">
+                        <div class="numbers">
+                            <p class="card-category">H. Leaders</p>
+                            <?php
+                            
+                                $tblquery = "SELECT COUNT(id) AS homecells FROM tbl_leaders WHERE type = 'H' AND status = '1'";
+                                $tblvalue = array();
+                                $select = $connect->tbl_select($tblquery, $tblvalue);
+                                foreach($select as $data){
+                                    extract($data);
+                                    echo "
+                                        <p class='card-title'>$homecells<p>
+                                    ";
+                                }
+                            
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                    <i class="fa fa-clock-o"></i>
+                    Active Homecell Leaders
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-body ">
+                <div class="row">
+                    <div class="col-5 col-md-4">
+                        <div class="icon-big text-center icon-warning">
+                            <i class="fa fa-university text-danger"></i>
+                        </div>
+                    </div>
+                    <div class="col-7 col-md-8">
+                        <div class="numbers">
+                            <p class="card-category">C. Leaders</p>
+                            <?php
+                            
+                                $tblquery = "SELECT COUNT(id) AS churches FROM tbl_leaders WHERE type = 'C' AND status = '1'";
+                                $tblvalue = array();
+                                $select = $connect->tbl_select($tblquery, $tblvalue);
+                                foreach($select as $data){
+                                    extract($data);
+                                    echo "
+                                        <p class='card-title'>$churches<p>
+                                    ";
+                                }
+                            
+                            ?>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                    <i class="fa fa-clock-o"></i>
+                    Active Church Leaders
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-body ">
+                <div class="row">
+                    <div class="col-5 col-md-4">
+                        <div class="icon-big text-center icon-warning">
+                            <i class="fa fa-bolt text-primary" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <div class="col-7 col-md-8">
+                        <div class="numbers">
+                            <p class="card-category">Admin</p>
+                            <?php
+                            
+                                $tblquery = "SELECT COUNT(id) AS admin FROM tbl_leaders WHERE type = 'A' AND status = '1'";
+                                $tblvalue = array();
+                                $select = $connect->tbl_select($tblquery, $tblvalue);
+                                foreach($select as $data){
+                                    extract($data);
+                                    echo "
+                                        <p class='card-title'>$admin<p>
+                                    ";
+                                }
+                            
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                    <i class="fa fa-clock-o"></i>
+                    Active Admin
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-body ">
+                <div class="row">
+                    <div class="col-5 col-md-4">
+                        <div class="icon-big text-center icon-warning">
+                            <i class="fa fa-home text-primary"></i>
+                        </div>
+                    </div>
+                    <div class="col-7 col-md-8">
+                        <div class="numbers">
+                            <p class="card-category">Homecells</p>
+                            <?php
+                            
+                                $tblquery = "SELECT COUNT(id) AS allHomecells FROM homecells";
+                                $tblvalue = array();
+                                $select = $connect->tbl_select($tblquery, $tblvalue);
+                                foreach($select as $data){
+                                    extract($data);
+                                    echo "
+                                        <p class='card-title'>$allHomecells<p>
+                                    ";
+                                }
+                            
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                    <i class="fa fa-clock-o"></i>
+                    All Homecells
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-body ">
+                <div class="row">
+                    <div class="col-5 col-md-4">
+                        <div class="icon-big text-center icon-warning">
+                            <i class="fa fa-university text-danger"></i>
+                        </div>
+                    </div>
+                    <div class="col-7 col-md-8">
+                        <div class="numbers">
+                            <p class="card-category">Churches</p>
+                            <?php
+                            
+                                $tblquery = "SELECT COUNT(id) AS churches FROM churches ";
+                                $tblvalue = array();
+                                $select = $connect->tbl_select($tblquery, $tblvalue);
+                                foreach($select as $data){
+                                    extract($data);
+                                    echo "
+                                        <p class='card-title'>$churches<p>
+                                    ";
+                                }
+                            
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                    <i class="fa fa-clock-o"></i>
+                    All Churches
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
