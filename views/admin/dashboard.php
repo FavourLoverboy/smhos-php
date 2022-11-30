ce023ad8ca017383a19ceff9bafea704cdfeec20
favourloverboy <favourehio2019@gmail.com>
2022-11-30 20:12:14 +0100

    admin see homecell attendance chart

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -235,8 +235,74 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-6">
+        <div class="col-12">
             <canvas id="myChart"></canvas>
         </div>
     </div>
+
+    <?php 
+        $themes = array();
+        $attendance = array();
+
+        $tblquery = "SELECT theme.theme AS t, COUNT(attendance.user) AS members FROM theme INNER JOIN attendance ON theme.id = attendance.theme_id WHERE attendance.h_id != '' GROUP BY attendance.theme_id  ORDER BY theme.id DESC LIMIT 5";
+        $tblvalue = array();
+        $select = $connect->tbl_select($tblquery, $tblvalue);
+        
+        foreach($select as $data){
+            extract($data);
+            array_push($themes, $t);
+            array_push($attendance, $members);
+        }
+    
+    ?>
+
+    <script>
+        const ctx = document.getElementById('myChart');
+
+        const plugin = {
+            id: 'customCanvasBackgroundColor',
+            beforeDraw: (chart, args, options) => {
+                const {ctx} = chart;
+                ctx.save();
+                ctx.globalCompositeOperation = 'destination-over';
+                ctx.fillStyle = options.color || '#99ffff';
+                ctx.fillRect(0, 0, chart.width, chart.height);
+                ctx.restore();
+            }
+        };
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: <?php echo json_encode($themes); ?>,
+                datasets: [{
+                    label: 'Last 5 Homecell attendance record',
+                    data: <?php echo json_encode($attendance); ?>,
+                    borderWidth: 1,
+                    backgroundColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(54, 162, 235)',
+                        'rgb(255, 205, 86)',
+                        'rgb(20, 290, 0)',
+                        'rgb(255, 95, 59)'
+                    ],
+                    hoverOffset: 4
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                plugins: {
+                    customCanvasBackgroundColor: {
+                        color: 'white',
+                    }
+                }
+            },
+            plugins: [plugin],
+        });
+    </script>
 </div>
+
