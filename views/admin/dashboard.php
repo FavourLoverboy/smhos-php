83342a6be4771a7d0b7ac7ed68ca42ac9f5056e2
favourloverboy <favourehio2019@gmail.com>
2022-11-30 21:33:32 +0100

    admin see sex chart

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -331,3 +331,71 @@
     </script>
 </div>
 
+<div class="container-fluid pt-5">
+    <div class="row">
+        <!-- Sex -->
+        <div class="col-md-6" style="height: 500px;">
+            <canvas id="membersSex" class="w-100"></canvas>
+        </div>
+
+        <?php
+        
+            $sex = array();
+
+            $tblquery = "SELECT COUNT(id) AS men FROM members WHERE sex = 'M'";
+            $tblvalue = array();
+            $men = $connect->tbl_select($tblquery, $tblvalue);
+            foreach($men as $data){
+                extract($data);
+                array_push($sex, $men);
+            }
+
+            $tblquery = "SELECT COUNT(id) AS women FROM members WHERE sex = 'F'";
+            $tblvalue = array();
+            $women = $connect->tbl_select($tblquery, $tblvalue);
+            foreach($women as $data){
+                extract($data);
+                array_push($sex, $women);
+            }
+        
+        ?>
+
+        <script>
+            const membersSex = document.getElementById('membersSex');
+
+            new Chart(membersSex, {
+                type: 'pie',
+                data: {
+                    labels: ['Men', 'Women'],
+                    datasets: [{
+                        data: <?php echo json_encode($sex); ?>,
+                        borderWidth: 1
+                    }]
+                },
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)'
+                ],
+                hoverOffset: 4,
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: 'top',
+                        },
+                        title: {
+                            display: true,
+                            text: 'Members base on Sex'
+                        }
+                    }
+                }
+            });
+        </script>
+
+
+        <div class="col-md-5 ml-5 bg-info" style="height: 500px;">
+        
+        </div>
+    </div>
+</div>
+
