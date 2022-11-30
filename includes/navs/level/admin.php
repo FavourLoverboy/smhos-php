8fc60488838f28d8b49dad046bd2b0362bb66d64
favourloverboy <favourehio2019@gmail.com>
2022-11-30 22:51:20 +0100

    admin analyse setup

--- includes/navs/level/admin.php
+++ includes/navs/level/admin.php
@@ -1,66 +1,95 @@
-<li class="active">
-    <a href="dashboard">
-        <i class="fa fa-dashboard"></i>
-        <p>Dashboard</p>
-    </a>
-</li>
-<li>
-    <a href="attendance">
-        <i class="fa fa-bar-chart"></i>
-        <p>Attendance</p>
-    </a>
-</li>
-<li>
-    <a href="churches">
-        <i class="fa fa-university"></i>
-        <p>Churches</p>
-    </a>
-</li>
-<li>
-    <a href="complains">
-        <i class="fa fa-comments"></i>
-        <p>Complains</p>
-    </a>
-</li>
-<li>
-    <a href="homecells">
-        <i class="fa fa-home"></i>
-        <p>Homecells</p>
-    </a>
-</li>
-<li>
-    <a href="homecell_materials">
-        <i class="fa fa-calendar"></i>
-        <p>Homecell Materials</p>
-    </a>
-</li>
-<li>
-    <a href="leaders">
-        <i class="fa fa-bolt"></i>
-        <p>Leaders</p>
-    </a>
-</li>
-<li>
-    <a href="members">
-        <i class="fa fa-users"></i>
-        <p>Members</p>
-    </a>
-</li>
-<li>
-    <a href="all_prayers">
-        <i class="fa fa-bullhorn"></i>
-        <p>Prayers</p>
-    </a>
-</li>
-<li>
-    <a href="testimonies">
-        <i class="fa fa-smile-o"></i>
-        <p>Testimonies</p>
-    </a>
-</li>
-<li>
-    <a href="themes">
-        <i class="fa fa-book"></i>
-        <p>Homecell Themes</p>
-    </a>
-</li>
+<?php
+
+    if($_SESSION['analyseStatus']){
+        echo "
+            <li>
+                <a href='dashboard'>
+                    <i class='fa fa-backward'></i>
+                    <p>Back</p>
+                </a>
+            </li>
+            <li>
+                <a href='attendance_chart'>
+                    <i class='fa fa-book'></i>
+                    <p>Homecell Themes</p>
+                </a>
+            </li>
+        ";
+    }else{
+        echo "
+            <li class='active'>
+                <a href='dashboard'>
+                    <i class='fa fa-dashboard'></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            <li>
+                <a href='analyse'>
+                    <i class='fa fa-pie-chart'></i>
+                    <p>Analyse</p>
+                </a>
+            </li>
+            <li>
+                <a href='attendance'>
+                    <i class='fa fa-bar-chart'></i>
+                    <p>Attendance</p>
+                </a>
+            </li>
+            <li>
+                <a href='churches'>
+                    <i class='fa fa-university'></i>
+                    <p>Churches</p>
+                </a>
+            </li>
+            <li>
+                <a href='complains'>
+                    <i class='fa fa-comments'></i>
+                    <p>Complains</p>
+                </a>
+            </li>
+            <li>
+                <a href='homecells'>
+                    <i class='fa fa-home'></i>
+                    <p>Homecells</p>
+                </a>
+            </li>
+            <li>
+                <a href='homecell_materials'>
+                    <i class='fa fa-calendar'></i>
+                    <p>Homecell Materials</p>
+                </a>
+            </li>
+            <li>
+                <a href='leaders'>
+                    <i class='fa fa-bolt'></i>
+                    <p>Leaders</p>
+                </a>
+            </li>
+            <li>
+                <a href='members'>
+                    <i class='fa fa-users'></i>
+                    <p>Members</p>
+                </a>
+            </li>
+            <li>
+                <a href='all_prayers'>
+                    <i class='fa fa-bullhorn'></i>
+                    <p>Prayers</p>
+                </a>
+            </li>
+            <li>
+                <a href='testimonies'>
+                    <i class='fa fa-smile-o'></i>
+                    <p>Testimonies</p>
+                </a>
+            </li>
+            <li>
+                <a href='themes'>
+                    <i class='fa fa-book'></i>
+                    <p>Homecell Themes</p>
+                </a>
+            </li>
+        ";
+    }
+
+?>
--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -1,3 +1,8 @@
+<?php
+
+    $_SESSION['analyseStatus'] = false;
+
+?>
 <div class="row">
     <div class="col-lg-3 col-md-6 col-sm-6">
         <div class="card card-stats">
