b455333d5f79d8280e2edf7fb345709606d73bf4
favourloverboy <favourehio2019@gmail.com>
2022-11-30 20:43:27 +0100

    updated admin attendance chart

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -233,7 +233,7 @@
     </div>
 </div>
 
-<div class="container-fluid">
+<div class="container-fluid pt-5">
     <div class="row">
         <div class="col-12">
             <canvas id="myChart"></canvas>
@@ -243,15 +243,27 @@
     <?php 
         $themes = array();
         $attendance = array();
+        $mem = array();
 
-        $tblquery = "SELECT theme.theme AS t, COUNT(attendance.user) AS members FROM theme INNER JOIN attendance ON theme.id = attendance.theme_id WHERE attendance.h_id != '' GROUP BY attendance.theme_id  ORDER BY theme.id DESC LIMIT 5";
+        $tblquery = "SELECT theme.id AS theme_id, theme.theme AS t, COUNT(attendance.user) AS members FROM theme INNER JOIN attendance ON theme.id = attendance.theme_id GROUP BY attendance.theme_id  ORDER BY theme.id DESC LIMIT 5";
         $tblvalue = array();
         $select = $connect->tbl_select($tblquery, $tblvalue);
         
         foreach($select as $data){
             extract($data);
-            array_push($themes, $t);
-            array_push($attendance, $members);
+
+            $tblquery = "SELECT COUNT(h_id) AS att FROM attendance WHERE theme_id = :theme_id AND h_id != ''";
+            $tblvalue = array(
+                ':theme_id' => $theme_id
+            );
+            $select12 = $connect->tbl_select($tblquery, $tblvalue);
+            foreach($select12 as $data){
+                extract($data);
+
+                array_push($themes, $t);
+                array_push($mem, $members);
+                array_push($attendance, $att);
+            }
         }
     
     ?>
@@ -276,8 +288,21 @@
             data: {
                 labels: <?php echo json_encode($themes); ?>,
                 datasets: [{
-                    label: 'Last 5 Homecell attendance record',
+                    label: 'Attendance',
                     data: <?php echo json_encode($attendance); ?>,
+                    borderWidth: 1,
+                    backgroundColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(54, 162, 235)',
+                        'rgb(255, 205, 86)',
+                        'rgb(20, 290, 0)',
+                        'rgb(255, 95, 59)'
+                    ],
+                    hoverOffset: 4
+                },
+                {
+                    label: 'Total Members',
+                    data: <?php echo json_encode($mem); ?>,
                     borderWidth: 1,
                     backgroundColor: [
                         'rgb(255, 99, 132)',
