008ad1e0fe1f7252a59483b18770670635b75e31
favourloverboy <favourehio2019@gmail.com>
2022-12-03 10:23:13 +0100

    member dashboard

--- views/member/dashboard.php
+++ views/member/dashboard.php
@@ -1,1 +1,185 @@
-member dashboard
+<?php
+
+    $_SESSION['analyseStatus'] = false;
+
+    $attendances = array();
+
+?>
+<div class="row">
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-body ">
+                <div class="row">
+                    <div class="col-5 col-md-4">
+                        <div class="icon-big text-center icon-warning">
+                            <i class="fa fa-sign-in text-warning"></i>
+                        </div>
+                    </div>
+                    <div class="col-7 col-md-8">
+                        <div class="numbers">
+                            <p class="card-category">Present</p>
+                            <?php
+                            
+                                $tblquery = "SELECT COUNT(id) AS presents FROM attendance WHERE user = :user_id AND h_id != ''";
+                                $tblvalue = array(
+                                    'user_id' => $_SESSION['myId']
+                                );
+                                $select = $connect->tbl_select($tblquery, $tblvalue);
+                                foreach($select as $data){
+                                    extract($data);
+                                    array_push($attendances, $presents);
+                                    echo "
+                                        <p class='card-title'>$presents<p>
+                                    ";
+                                }
+                            
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                    <i class="fa fa-clock-o"></i>
+                    Present
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-body ">
+                <div class="row">
+                    <div class="col-5 col-md-4">
+                        <div class="icon-big text-center icon-warning">
+                            <i class="fa fa-sign-out text-success"></i>
+                        </div>
+                    </div>
+                    <div class="col-7 col-md-8">
+                        <div class="numbers">
+                            <p class="card-category">Absent</p>
+                            <?php
+                            
+                                $tblquery = "SELECT COUNT(id) AS absent FROM attendance WHERE user = :user_id AND h_id = ''";
+                                $tblvalue = array(
+                                    'user_id' => $_SESSION['myId']
+                                );
+                                $select = $connect->tbl_select($tblquery, $tblvalue);
+                                foreach($select as $data){
+                                    extract($data);
+                                    array_push($attendances, $absent);
+                                    echo "
+                                        <p class='card-title'>$absent<p>
+                                    ";
+                                }
+                            
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                    <i class="fa fa-clock-o"></i>
+                    Absent
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-body ">
+                <div class="row">
+                    <div class="col-5 col-md-4">
+                        <div class="icon-big text-center icon-warning">
+                            <i class="fa fa-smile-o text-danger"></i>
+                        </div>
+                    </div>
+                    <div class="col-7 col-md-8">
+                        <div class="numbers">
+                            <p class="card-category">H. Event</p>
+                            <?php
+                            
+                                $tblquery = "SELECT COUNT(id) AS total FROM attendance WHERE user = :user_id";
+                                $tblvalue = array(
+                                    'user_id' => $_SESSION['myId']
+                                );
+                                $select = $connect->tbl_select($tblquery, $tblvalue);
+                                foreach($select as $data){
+                                    extract($data);
+                                    echo "
+                                        <p class='card-title'>$total<p>
+                                    ";
+                                }
+                            
+                            ?>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                    <i class="fa fa-clock-o"></i>
+                    All Homecell Events
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid pt-5">
+    <div class="row">
+        <div class="col-12">
+            <canvas id="myChart"></canvas>
+        </div>
+    </div>
+
+    <script>
+        const ctx = document.getElementById('myChart');
+
+        const plugin = {
+            id: 'customCanvasBackgroundColor',
+            beforeDraw: (chart, args, options) => {
+                const {ctx} = chart;
+                ctx.save();
+                ctx.globalCompositeOperation = 'destination-over';
+                ctx.fillStyle = options.color || '#99ffff';
+                ctx.fillRect(0, 0, chart.width, chart.height);
+                ctx.restore();
+            }
+        };
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Present', 'Absent'],
+                datasets: [{
+                    label: 'Total Attendance Chart',
+                    data: <?php echo json_encode($attendances); ?>,
+                    borderWidth: 1,
+                    backgroundColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(54, 162, 235)'
+                    ],
+                    hoverOffset: 4
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                plugins: {
+                    customCanvasBackgroundColor: {
+                        color: 'white',
+                    }
+                }
+            },
+            plugins: [plugin],
+        });
+    </script>
+</div>
